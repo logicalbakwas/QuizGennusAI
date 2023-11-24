dc7c1fe4bcbf60748db62ccbf9cd07d53d4c765f
Abdullah <aabdullahmian@gmail.com>
2023-11-24 19:24:00 +0500

    starting..

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+ 
+class QuizGennusAI
+ {
+     public:
+     void start()
+     {
+         cout<<"Hello World";
+     }
+ };
+ int main()
+ {
+     QuizGennusAI obj;
+     obj.start();
+     return 0;
+ }
